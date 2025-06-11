8d1f1cf36ae15e09239f3d9be97ff3e1af3c3dde
Carlos Frias <carlos.a.frias@gmail.com>
2025-06-11 17:45:06 -0300

    chore: add validation to create user endpoint

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,27 @@
+import {
+	IsEmail,
+	IsNotEmpty,
+	IsOptional,
+	Matches,
+	MaxLength,
+	MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+	@IsNotEmpty()
 	authority: string;
+
+	@IsNotEmpty()
+	@Matches(/^[a-zA-Z0-9]+$/)
+	@MinLength(3)
+	@MaxLength(30)
 	userName: string;
+
+	@IsEmail()
+	@IsNotEmpty()
 	email: string;
+
+	@IsOptional()
+	@MaxLength(30)
 	displayName?: string;
 }
